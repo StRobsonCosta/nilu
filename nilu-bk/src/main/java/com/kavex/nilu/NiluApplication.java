7366602a03021aa2645d8bb2afa3c0438849aac6
Robson Costa <st.robson.costa@gmail.com>
2025-06-25 22:47:19 -0300

    first commit

---
+++ nilu-bk/src/main/java/com/kavex/nilu/NiluApplication.java
@@ -0,0 +1,13 @@
+package com.kavex.nilu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiluApplication.class, args);
+	}
+
+}
---
+++ nilu-bk/src/test/java/com/kavex/nilu/NiluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kavex.nilu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
