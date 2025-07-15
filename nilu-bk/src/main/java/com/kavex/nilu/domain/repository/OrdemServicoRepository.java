cec31a027837f1d1fd313603be641d7c241c3010
Robson Costa <st.robson.costa@gmail.com>
2025-07-15 20:24:28 -0300

    repository do OS

--- nilu-bk/src/main/java/com/kavex/nilu/domain/repository/OrdemServicoRepository.java
+++ nilu-bk/src/main/java/com/kavex/nilu/domain/repository/OrdemServicoRepository.java
@@ -1,13 +1,30 @@
 package com.kavex.nilu.domain.repository;
 
 import com.kavex.nilu.domain.model.OrdemServico;
+import com.kavex.nilu.dto.RelatorioFinanceiroDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface OrdemServicoRepository extends JpaRepository<OrdemServico, UUID> {
+
+    @Query("""
+    SELECT new br.com.ateliecostura.dto.RelatorioFinanceiroDto(
+        c.nome,
+        SUM(o.valorTotal),
+        SUM(o.comissaoCostureira)
+    )
+    FROM OrdemServico o
+    JOIN o.costureira c
+    WHERE o.dataRecebimento BETWEEN :inicio AND :fim
+    GROUP BY c.nome
+""")
+    List<RelatorioFinanceiroDto> gerarRelatorio(LocalDate inicio, LocalDate fim);
 }
