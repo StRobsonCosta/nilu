f057c95aa6eef67c9582828499fafdedb9088aaf
Robson Costa <st.robson.costa@gmail.com>
2025-07-29 21:51:21 -0300

    TF lambdas e gateway

---
+++ nilu-bk/infra/terraform/lambda.tf
@@ -0,0 +1,35 @@
+resource "aws_iam_role" "lambda_exec" {
+  name = "${var.project_name}-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "api_lambda" {
+  function_name = "${var.project_name}-lambda"
+  role          = aws_iam_role.lambda_exec.arn
+  handler       = "org.springframework.cloud.function.adapter.aws.FunctionInvoker::handleRequest"
+  runtime       = "java17"
+
+  filename         = "../../target/esteira-costura-1.0.0.jar"
+  source_code_hash = filebase64sha256("../../target/esteira-costura-1.0.0.jar")
+
+  environment {
+    variables = {
+      SPRING_PROFILES_ACTIVE = "lambda"
+    }
+  }
+}
